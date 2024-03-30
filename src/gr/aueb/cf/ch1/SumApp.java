952ddd251dd4f34f2587f430430fe90ed6f3a0c1
Kanelloss <163835990+Kanelloss@users.noreply.github.com>
2024-03-30 17:43:59 +0200

    23
    25436

---
+++ src/gr/aueb/cf/ch1/SumApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch1;
+
+public class SumApp {
+    public static void main(String[] args) {
+
+        int num1 = 19; //Declaring first variable
+        int num2 = 30; // Declaring second variable
+        int sum = num1 + num2; // Declaring summary variable
+        System.out.println("Το αποτέλεσμα της πρόσθεσης είναι ίσο με: " + sum); //Printing result
+    }
+}
