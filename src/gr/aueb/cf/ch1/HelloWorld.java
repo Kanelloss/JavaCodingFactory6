839c2a7b222ed44895886d55a9c783a62102db4f
Kanelloss <163835990+Kanelloss@users.noreply.github.com>
2024-04-01 18:50:07 +0300

    1

--- src/gr/aueb/cf/ch1/HelloWorld.java
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -1,10 +1,20 @@
 package gr.aueb.cf.ch1;
 
+/**
+ (documentation comments or java doc or doc comments)
+ Εκτυπώνει 'Hello World στο standard output (κονσόλα)
+ *
+ */
 public class HelloWorld {
-
     public static void main(String[] args) {
-        int i = 0, j = 1;
-        j = i + 1;
-        System.out.println("i=" + i +  " " + "j=” + j)
+
+        //Prints Hello World and then writes a new line.
+
+        /*
+        (Implementation Comment or Block Comment)
+         * Prints Hello World and then writes a new line.
+         */
+        System.out.println("HelloWorld!");
     }
 }
+
---
+++ src/gr/aueb/cf/ch2/Notes.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch2;
+
+/**
+  (documentation comments or java doc or doc comments)
+  Εκτυπώνει 'Hello World στο standard output (κονσόλα)
+ *
+ */
+public class Notes {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
