c4e1456b682547516b6bf68cc43a52861fee86c1
Kanelloss <163835990+Kanelloss@users.noreply.github.com>
2024-04-06 19:37:47 +0300

    Update abc.java

--- src/gr/aueb/cf/ch2/abc.java
+++ src/gr/aueb/cf/ch2/abc.java
@@ -3,11 +3,8 @@ package gr.aueb.cf.ch2;
 public class abc {
 
     public static void main(String[] args) {
-        int num1 = 5;
-        long num2 = 3_000_000_000_000L;
-        System.out.println(num2);
-        int num3 = (int) num2;
-        System.out.println(num2);
+        int num = 5L/9;
+        System.out.printf("%04d",num);
 
     }
 }
