2acf073abe100fecfe659018791cf359bc0530d7
Kanelloss <163835990+Kanelloss@users.noreply.github.com>
2024-04-17 15:32:32 +0300

    ch5

--- src/gr/aueb/cf/ch4/CharOrdinalApp.java
+++ src/gr/aueb/cf/ch4/CharOrdinalApp.java
@@ -6,8 +6,8 @@ package gr.aueb.cf.ch4;
 public class CharOrdinalApp {
 
     public static void main(String[] args) {
-        char ch1 = 'a';
-        char ch2 = '#';
+        char ch1 = 's';
+        char ch2 = '8';
 
         System.out.printf("char: %c, ordinal: %d\n", ch1, (int) ch1);
         System.out.printf("char: %c, ordinal: %d", ch2, (int) ch2);
