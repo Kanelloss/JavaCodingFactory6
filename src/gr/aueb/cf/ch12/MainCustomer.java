0d97a80953e51770e36cfefbefd48b7bce9d3ef6
Kanellos Almpanis <kanellosalmpanis@hotmail.com>
2024-05-27 21:01:26 +0300

    ch12 1st lesson

---
+++ src/gr/aueb/cf/ch12/MainCustomer.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch12;
+
+import gr.aueb.cf.ch12.model.Customer;
+
+public class MainCustomer {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.setId(1);
+        customer.setVatRegistrationNo("123456789");
+
+        Customer anna = new Customer(2, "Anna", "G.", "234567890", "699999999", "Attica", "Central Attica", "Athens", "Patission", "76", "10434");
+
+        System.out.println("Anna's Vat: " + anna.getVatRegistrationNo());
+        System.out.println("Customer's Vat: " + customer.getVatRegistrationNo());
+    }
+}
