3c4909aa3ba098263549235ed9f6b77024237ae6
Kanellos Almpanis <kanellosalmpanis@hotmail.com>
2024-06-03 20:16:12 +0300

    ch14

---
+++ src/gr/aueb/cf/ch14/PointMain.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch14;
+
+public class PointMain {
+
+    public static void main(String[] args) {
+        Point zeroPoint = Point.getZeroPoint();
+        Point randomPoint = Point.getRandomPoint();
+
+        Point point = new Point();
+    }
+}
