f82097e15f20e9824208b9a402aeacd67d7316ac
Kanellos Almpanis <kanellosalmpanis@hotmail.com>
2024-06-03 20:57:09 +0300

    14

---
+++ src/gr/aueb/cf/ch15/Point.java
@@ -0,0 +1,39 @@
+package gr.aueb.cf.ch15;
+
+public class Point {
+    protected double x;
+
+    public Point() {
+
+    }
+
+    public Point(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public String convertToString() {
+        return "(" + x + ")";
+    }
+
+    // API
+
+    public  void movePlus10() {
+        x += 10;
+    }
+
+    public void movePlusOne() {
+        x++;
+    }
+
+    protected void reset() {
+        x = 0;
+    }
+}
---
+++ src/gr/aueb/cf/ch15/Point2D.java
@@ -0,0 +1,41 @@
+package gr.aueb.cf.ch15;
+
+public class Point2D extends Point {
+    protected double y;
+
+    public Point2D() {
+        // super();        // Points to super class (Point).
+        // y = 0;       // Automatically JVM does these two.
+    }
+
+    public Point2D(double x, double y) {
+        super(x);
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String convertToString() {
+        return "(" + getX() + ", " + y + ")";
+    }
+
+    
+    // Public API
+
+    public  void movePlus10() {
+        x += 10;
+    }
+
+    public void movePlusOne() {
+        x++;
+    }
+
+
+}
