0a83ee7e06812e43b2ceeda0196a3ad115b63f93
Kanellos Almpanis <kanellosalmpanis@hotmail.com>
2024-07-03 19:20:53 +0300

    streams

--- src/gr/aueb/cf/ch19/sorting/Main.java
+++ src/gr/aueb/cf/ch19/sorting/Main.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Product> products = new ArrayList<>(List.of(new Product("Milk", 8.50, 100), new Product("Honey", 8.50, 50),
-                new Product("Oranges", 8.50, 20), new Product("Apples", 8.50, 20)));
+        List<Product> products = new ArrayList<>(List.of(new Product("Milk", 2.25, 100), new Product("Apples", 8.50, 50),
+                new Product("Apples", 8.50, 20), new Product("Apples", 8.50, 20)));
 
 //        System.out.println("Ascending Sorting");
 //        Collections.sort(products);     // Comparable was used
@@ -35,7 +35,19 @@ public class Main {
 //        products.sort(Comparator.comparing(Product::getDescription));       // Works without Comparable
 //        products.forEach(System.out::println);
 
-        products.sort(Comparator.comparing(Product::getPrice).thenComparing(Product::getQuantity).thenComparing(Product::getDescription));  // Check this one out. Important.
+//        products.sort(Comparator.comparing(Product::getPrice).thenComparing(Product::getQuantity));
+//        products.forEach(System.out::println);
+
+//        products.sort(Comparator.comparing(Product::getDescription)
+//                .thenComparingDouble(Product::getPrice)
+//                .thenComparingInt(Product::getQuantity));  // Check this one out. Important.
+//
+//        products.forEach(System.out::println);
+
+        products.sort(Comparator.comparing(Product::getDescription)
+                .thenComparing(Product::getPrice, Comparator.reverseOrder())
+                .thenComparing(Product::getQuantity));
+
         products.forEach(System.out::println);
 
     }
