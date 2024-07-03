d3cf99f06f44445900ded2c54f86ecf3972bb401
Kanellos Almpanis <kanellosalmpanis@hotmail.com>
2024-07-03 19:42:02 +0300

    Create Main.java

---
+++ src/gr/aueb/cf/ch19/streams/Main.java
@@ -0,0 +1,48 @@
+package gr.aueb.cf.ch19.streams;
+
+import gr.aueb.cf.ch19.sorting.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>(List.of(new Product("Milk", 2.25, 100), new Product("Honey", 3.50, 50),
+                new Product("Honey", 8.50, 200), new Product("Oranges", 8.50, 20)));
+
+        // Η filter δεν πειράζει την ίδια λίστα, δημιουργεί μια καινούρια)
+//        List<Product> honey = products.stream()
+//                .filter(product -> product.getDescription().equals("Honey"))
+//                .toList();
+//
+//        honey.forEach(System.out::println);
+
+//        List<Product> sortedHoney = products.stream()
+//                .filter(product -> product.getDescription().equals("Honey"))
+//                .sorted(Comparator.comparing(Product::getPrice))
+//                .toList();
+//
+//        sortedHoney.forEach(System.out::println);
+
+//        List<Double> productPrices = products.stream()
+//                .sorted(Comparator.comparing(Product::getPrice, Comparator.reverseOrder()))
+//                .map(Product::getPrice)
+//                .toList();
+//
+//        productPrices.forEach(System.out::println);
+
+        List<Product> increasedPricesProducts = products.stream()
+                .map(p -> new Product(p.getDescription(), p.getPrice() + p.getPrice() * 0.1, p.getQuantity()))
+                .toList();
+
+        increasedPricesProducts.forEach(System.out::println);
+
+
+
+
+    }
+}
