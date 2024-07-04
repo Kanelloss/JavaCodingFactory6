60f170d29e3d5494583ac2a5121155bfb123cd31
Kanellos Almpanis <kanellosalmpanis@hotmail.com>
2024-07-04 19:33:44 +0300

    add IAccountService

---
+++ src/gr/aueb/cf/ch18/accountapp/service/IAccountService.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch18.accountapp.service;
+
+
+import gr.aueb.cf.ch18.accountapp.dto.AccountDto;
+
+import java.util.List;
+
+public interface IAccountService {
+        void createAccount(AccountDto);
+        AccountDto getAccount(int id);
+        void updateAccount(AccountDto accountDto);
+        void deleteAccount(int id);
+        List<AccountDto> listAccounts();
+    }
